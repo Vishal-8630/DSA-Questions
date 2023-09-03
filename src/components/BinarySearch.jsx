a0d8fbf3b77e1664dbb87beae14cd5864a3152bc
= <=>
2023-09-03 20:29:24 +0530

    Binary search questions added

--- src/components/BinarySearch.jsx
+++ src/components/BinarySearch.jsx
@@ -1,17 +1,24 @@
 import React from 'react'
-import questions from '../data/binary_search'
+import {questions_type_1, questions_type_2} from '../data/binary_search'
 
 const BinarySearch = () => {
 
-    const questionElement = questions.map((question, index) => {
+    const questionElementType1 = questions_type_1.map((question, index) => {
         return <li key={index}> <a href={question} target='_blank'> {index + 1}. { question }</a> </li>
-    })
+    });
+
+    const questionElementType2 = questions_type_2.map((question, index) => {
+        return <li key={index}> <a href={question} target='_blank'> {index + 1}. { question }</a> </li>
+    });
 
   return (
     <div>
         <h1>Binary Search</h1>
         <ul>
-            {questionElement}
+            <h3>Type 1</h3>
+            { questionElementType1 }
+            <h3>Type 2</h3>
+            { questionElementType2 }
         </ul>
     </div>
   )
--- src/data/binary_search.js
+++ src/data/binary_search.js
@@ -1,6 +1,23 @@
-let questions = [
+export let questions_type_1 = [
     "https://leetcode.com/problems/binary-search/?envType=study-plan-v2&envId=binary-search",
-    "https://leetcode.com/problems/search-insert-position/?envType=study-plan-v2&envId=binary-search"
+    "https://leetcode.com/problems/search-insert-position/?envType=study-plan-v2&envId=binary-search",
+    "https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/?envType=study-plan-v2&envId=binary-search",
+    "https://leetcode.com/problems/search-in-rotated-sorted-array/?envType=study-plan-v2&envId=binary-search",
+    "https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/?envType=study-plan-v2&envId=binary-search",
+    "https://leetcode.com/problems/single-element-in-a-sorted-array/?envType=study-plan-v2&envId=binary-search",
+    "https://leetcode.com/problems/peak-index-in-a-mountain-array/?envType=study-plan-v2&envId=binary-search",
+    "https://leetcode.com/problems/guess-number-higher-or-lower/?envType=study-plan-v2&envId=binary-search",
+    "https://leetcode.com/problems/first-bad-version/?envType=study-plan-v2&envId=binary-search",
+    "https://leetcode.com/problems/kth-missing-positive-number/?envType=study-plan-v2&envId=binary-search"
 ]
 
-export default questions;
+export let questions_type_2 = [
+    "https://leetcode.com/problems/valid-perfect-square/?envType=study-plan-v2&envId=binary-search",
+    "https://leetcode.com/problems/sqrtx/?envType=study-plan-v2&envId=binary-search",
+    "https://leetcode.com/problems/arranging-coins/?envType=study-plan-v2&envId=binary-search",
+    "https://leetcode.com/problems/successful-pairs-of-spells-and-potions/?envType=study-plan-v2&envId=binary-search",
+    "https://leetcode.com/problems/koko-eating-bananas/",
+    "https://leetcode.com/problems/find-the-smallest-divisor-given-a-threshold/",
+    "https://leetcode.com/problems/minimum-number-of-days-to-make-m-bouquets/",
+    "https://leetcode.com/problems/magnetic-force-between-two-balls/",
+]
